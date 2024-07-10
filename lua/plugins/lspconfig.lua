f8b0f800a6a02d89e0bd7ce78c2269cad686af4b
Linus Almgren <linus.almgren@gmail.com>
2024-07-11 01:43:22 +0200

    delete luasnip and update cmp config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -10,21 +10,20 @@ return {
       { -- replaces folke/neodev
         'folke/lazydev.nvim',
         ft = 'lua', -- only load on lua files
+        cmd = 'LazyDev',
         opts = {
           library = {
-            'luvit-meta/library',
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+            { path = 'LazyVim', words = { 'LazyVim' } },
+            { path = 'lazy.nvim', words = { 'LazyVim' } },
           },
         },
       },
       { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
-      { -- optional completion source for require statements and module annotations
+      {
         'hrsh7th/nvim-cmp',
         opts = function(_, opts)
-          opts.sources = opts.sources or {}
-          table.insert(opts.sources, {
-            name = 'lazydev',
-            group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-          })
+          table.insert(opts.sources, { name = 'lazydev', group_index = 0 })
         end,
       },
       { 'b0o/SchemaStore.nvim', version = false, lazy = true },
