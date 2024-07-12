716b3a8b1a4cbc5b5044ab41e3a28f0fc7954366
Linus Almgren <linus.almgren@gmail.com>
2024-07-12 03:59:06 +0200

    update some keymaps, add custom persistence keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,21 +8,21 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous [d]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next [d]iagnostic message' })
-vim.keymap.set('n', '<leader>m', vim.diagnostic.open_float, { desc = 'Show diagnostic error [m]essages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [q]uickfix list' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous [d]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next [d]iagnostic message' })
+-- vim.keymap.set('n', '<leader>m', vim.diagnostic.open_float, { desc = 'Show diagnostic error [m]essages' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>.
 -- This won't work in all terminal emulators/tmux/etc.
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
--- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- TIP: Disable arrow keys in normal mode - change window size instead
+vim.keymap.set('n', '<left>', ':vertical resize -5<cr>', { silent = true })
+vim.keymap.set('n', '<right>', ':vertical resize +5<cr>', { silent = true })
+vim.keymap.set('n', '<up>', ':resize +5<cr>', { silent = true })
+vim.keymap.set('n', '<down>', ':resize -5<cr>', { silent = true })
 
 -- Option key workaround
 -- vim.keymap.set('n', '√', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
@@ -32,7 +32,7 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 -- vim.keymap.set('v', '√', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
 -- vim.keymap.set('v', 'ª', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
 
--- Use CTRL+<hjkl> to switch between windows
+-- Use CTRL+<hjkl> to switch between windows - Set up with tmux navigator instead
 -- See `:help wincmd` for a list of all window commands
 -- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -5,56 +5,58 @@ return {
   dependencies = { 'catppuccin/nvim' },
   config = function() -- This is the function that runs, AFTER loading
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 200
 
     -- make sure to run this code before calling setup()
     -- refer to the full lists at https://github.com/folke/which-key.nvim/blob/main/lua/which-key/plugins/presets/init.lua
-    local presets = require 'which-key.plugins.presets'
-    presets.motions['['] = 'Previous'
-    presets.motions[']'] = 'Next'
-    presets.motions['g'] = 'Goto'
+
+    -- NOTE: maybe messes up stuff
+    -- local presets = require 'which-key.plugins.presets'
+    -- presets.motions['['] = 'Previous'
+    -- presets.motions[']'] = 'Next'
+    -- presets.motions['g'] = 'Goto'
 
     -- setup and document key chains
     local wk = require 'which-key'
     wk.setup {
-      -- add operators that will trigger motion and text object completion
-      operators = { gc = 'Line [c]omments', gb = '[b]lock comments' },
       key_labels = {
         ['<space>'] = 'SPC',
         ['<tab>'] = 'TAB',
       },
       window = {
-        border = 'shadow',
-        margin = { 0, 0, 0, 0 },
-        padding = { 1, 0, 0, 0 },
+        border = 'none',
+        margin = { 0, 0, 1, 0 },
+        padding = { 1, 0, 1, 0 },
       },
       hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', '^:', '^ ', '^call ', '^lua ' }, -- hide mapping boilerplate
       show_help = false, -- show a help message in the command line for using WhichKey
-      show_keys = false, -- show the currently pressed key and its label as a message in the command line
+      show_keys = true, -- show the currently pressed key and its label as a message in the command line
     }
 
     wk.register {
-      ['<leader>b'] = { name = '[b]uffers', _ = 'which_key_ignore' },
-      ['<leader>c'] = { name = '[c]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[d]ocument', _ = 'which_key_ignore' },
-      ['<leader>f'] = { name = '[f]ind', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = 'Lazy[g]it', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [h]unk', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[r]efactor', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[s]earch', _ = 'which_key_ignore' },
-      ['<leader>u'] = { name = '[u]til/toggle', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[w]orkspace', _ = 'which_key_ignore' },
-      ['<leader>x'] = { name = 'Fi[x] trouble', _ = 'which_key_ignore' },
-      ['<leader><tab>'] = { name = '[Tab]s', _ = 'which_key_ignore' },
+      ['<leader>b'] = { name = '[b]uffers' },
+      ['<leader>c'] = { name = '[c]ode' },
+      ['<leader>d'] = { name = '[d]ocument' },
+      ['<leader>f'] = { name = '[f]ind' },
+      ['<leader>g'] = { name = 'Lazy[g]it' },
+      ['<leader>h'] = { name = 'Git [h]unk' },
+      ['<leader>p'] = { name = '[p]ersitence' },
+      ['<leader>r'] = { name = '[r]efactor' },
+      ['<leader>s'] = { name = '[s]earch' },
+      ['<leader>u'] = { name = '[u]til/toggle' },
+      ['<leader>w'] = { name = '[w]orkspace' },
+      ['<leader>x'] = { name = 'Fi[x] trouble' },
+      ['<leader><tab>'] = { name = '[Tab]s' },
       ['gs'] = { name = '[s]urround' },
+      ['gc'] = { name = '[c]omments' },
+      ['gb'] = { name = '[b]block comments' },
     }
 
     -- visual mode
     wk.register({
-      ['<leader>c'] = { 'Format [c]ode' },
-      ['<leader>h'] = { 'Git [h]unk' },
-      ['gs'] = { '[s]urround' },
-      ['<leader>s'] = { '[s]earch' },
+      ['<leader>h'] = { name = 'Git [h]unk' },
+      ['gs'] = { name = '[s]urround' },
+      ['<leader>s'] = { name = '[s]earch' },
     }, { mode = 'v' })
   end,
 }
